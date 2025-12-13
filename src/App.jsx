0d4269c13543a4cea6e41c4c698c91f63bbf8052
ryanhenrqq <ryanh8116@gmail.com>
2025-12-13 20:26:33 -0300

    chore: fixing broken ui and cleaning the code

--- src/App.jsx
+++ src/App.jsx
@@ -7,53 +7,56 @@ function App() {
   return (
     <>
       <div className="header">
-        <h1 className="title">Calculadora</h1>
+        <h1 className="title">Kalc</h1>
       </div>
       <div className="contents">
         <div className="number-display">0</div>
         <div className='row-1'>
-          <div className='number-7'>
+          <div className='number-7 button-outside'>
             <button>7</button>
           </div>
-          <div className='number-8'>
+          <div className='number-8 button-outside'>
             <button>8</button>
           </div>
-          <div className='number-9'>
+          <div className='number-9 button-outside'>
             <button>9</button>
           </div>
         </div>
         <div className='row-2'>
-          <div className='number-4'>
+          <div className='number-4 button-outside'>
             <button>4</button>
           </div>
-          <div className='number-5'>
+          <div className='number-5 button-outside'>
             <button>5</button>
           </div>
-          <div className='number-6'>
+          <div className='number-6 button-outside'>
             <button>6</button>
           </div>
         </div>
         <div className='row-3'>
-          <div className='number-1'>
+          <div className='number-1 button-outside'>
             <button>1</button>
           </div>
-          <div className='number-2'>
+          <div className='number-2 button-outside'>
             <button>2</button>
           </div>
-          <div className='number-3'>
+          <div className='number-3 button-outside'>
             <button>3</button>
           </div>
         </div>
         <div className='row-4'>
-          <div className='number-0'>
+          <div className='number-0 button-outside'>
             <button>0</button>
           </div>
-          <div className='symbol-dot'>
+          <div className='symbol-dot button-outside'>
             <button>.</button>
           </div>
-          <div className='symbol-equals'>
+          <div className='symbol-equals button-outside'>
             <button>=</button>
           </div>
+        </div>
+        <div className='row-5'>
+          <button>Formulas</button>
         </div>
       </div>
     </>
