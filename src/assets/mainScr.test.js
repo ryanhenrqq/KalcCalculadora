b9962d2f2ae7a4578032e0adce5b5dcd79cad120
ryanhenrqq <ryanh8116@gmail.com>
2026-02-15 23:58:08 -0300

    internal: testing unitary test implementation and drafts

---
+++ src/assets/mainScr.test.js
@@ -0,0 +1,4 @@
+import { NumberPad } from "./mainScr";
+import { render, screen } from '@testing-library/react'
+
+// this is a draft, im learning unitary tests for the first time
---
+++ src/assets/testSetup.jsx
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom'
+
+// this is a draft, im learning unitary tests for the first time
