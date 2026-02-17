4d23d14c0b53656cf59b1009401613112a5ce91e
ryanhenrqq <ryanh8116@gmail.com>
2026-02-16 22:52:14 -0300

    feat alpha: implementing jest, and drafting inconsistent versions bug

---
+++ src/assets/mainScr.test.jsx
@@ -0,0 +1,15 @@
+import { NumberPad, NumPadButton } from "./mainScr";
+import { render, screen, fireEvent } from '@testing-library/react'
+//BUGS:
+    //REACT E REACT-DOM EM VERSOES DIFERENTES, CONFERIR NO NPM RUN DEV
+    //FAZER O PRIMEIRO TESTE UNITÁRIO APOS CORRIGIR O PROBLEMA DE VERSOES
+test("Funcionando numpad", ()=> {
+    const mockClick = jest.fn()
+    render(<NumPadButton label="clickhere" classname="number-0" onclick={mockClick} />)
+
+    const firerClik = screen.getByText(/clickhere/i)
+    fireEvent.click(firerClik)
+
+    expect(fireEvent).toBeInTheDocument()
+    expect(mockClick).toHaveBeenCalledTimes(1)
+})
